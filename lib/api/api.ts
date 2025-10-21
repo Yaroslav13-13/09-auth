08bac86ca12995476856ed5148e2bb6fd85b7773
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-10-21 22:15:16 +0300

    09-auth

---
+++ lib/api/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE = process.env.NEXT_PUBLIC_API_URL;
+if (!BASE) {
+  console.warn("⚠️ NEXT_PUBLIC_API_URL is not defined");
+}
+
+export const api = axios.create({
+  baseURL: BASE ?? "https://notehub-api.goit.study",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
