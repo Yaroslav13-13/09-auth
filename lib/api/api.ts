09daf086690d8424c67c0ba5935ddea3f6e96e1e
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-10-26 01:18:23 +0300

    Fixed mentor comments

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,25 @@
+// import axios from "axios";
+
+// const baseURL =
+//   process.env.NODE_ENV === "development"
+//     ? "/api"
+//     : process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ||
+//       "https://notehub-api.goit.study";
+
+// export const api = axios.create({
+//   baseURL,
+//   withCredentials: true,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
 import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "/api"
-    : process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ||
-      "https://notehub-api.goit.study";
+    : process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") + "/api";
 
 export const api = axios.create({
   baseURL,
